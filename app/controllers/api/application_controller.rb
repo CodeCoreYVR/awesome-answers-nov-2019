642e937ab32014555a1e849201c1344773f7685c
Hano <hinsul3113@gmail.com>
2020-02-18 10:14:45 -0800

    Handle all API validation errors in a generic way

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -9,6 +9,16 @@ class Api::ApplicationController < ApplicationController
     # from making such requests to our Rails app. It's a security measure
     # that is unneccessary in the context of a Web API.
     skip_before_action(:verify_authenticity_token)
+    # https://apidock.com/rails/ActiveSupport/Rescuable/ClassMethods/rescue_from
+    # rescue_from(<error-class>, with: <method-name>)
+    # Use 'rescue_from' in a controller to prevent
+    # an error of class <error-class> from crashing your 
+    # Rails application and do something else instead.
+
+    # The <method-name> is a method that will do the handle error
+    # and do whatever you need instead of crashing
+
+    rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
 
     private 
 
@@ -16,5 +26,32 @@ class Api::ApplicationController < ApplicationController
         unless current_user.present? 
             render json: { status: 401 }, status: 401
         end
+    end
+
+    def record_invalid(error) 
+        # {
+        #    errors: [
+        #        {
+        #            type: "ActiveRecord::InvalidRecord",
+        #            record_type: "Question",
+        #            field: "body",
+        #            message: "error message"
+        #        }   
+        #    ]
+        # }
+        invalid_record = error.record
+        errors = invalid_record.errors.map do |field, message|
+            {
+                type: error.class.to_s,
+                record_type: record.class.to_s,
+                field: field,
+                message: message
+            }
+        end
+
+        render(
+            json: { status: 422, errors: errors },
+            status: 422
+        )
     end
 end
