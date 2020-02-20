ea578edc2cfdbe434d891c53a5abf3a8b6a28c5a
Hano <hinsul3113@gmail.com>
2020-02-20 09:41:14 -0800

    Setup ActiveStorage and add avatar for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -51,6 +51,21 @@ class User < ApplicationRecord
     has_many :sent_gifts, class_name: 'Gift', foreign_key: :sender_id, dependent: :nullify
     has_many :received_gifts, class_name: 'Gift', foreign_key: :receiver_id, dependent: :nullify
 
+    # To learn more about ActiveStorage, go to:
+    # https://edgeguides.rubyonrails.org/active_storage_overview.html
+
+    # To Setup attachments or uploads, first generate
+    # a migration to create tables needed by 
+    # ActiveStorage:
+    # rails active_storage:install
+
+    # Then, run your migration
+
+    # To Support multiple file attachments, do:
+    # has_many_attached(:avatars)
+    # To support a single file attachemnt, do:
+    has_one_attached(:avatar)
+
 
     def full_name
         "#{first_name} #{last_name}".strip.squeeze
